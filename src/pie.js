ecb1166ad723068af2ebbb62e067397c36a6ed96
joaosobanski <54563534+joaosobanski@users.noreply.github.com>
2022-08-30 20:37:51 -0300

    c

--- src/pie.js
+++ src/pie.js
@@ -2,61 +2,40 @@
 import React, { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
 function Piechart() {
-    const [stdudentSubject, setStudentsubject] = useState([]);
-    const [studentMarks, setStudentMarks] = useState([]);
-
-    useEffect(() => {
-        const sSubject = [];
-        const sMarks = [];
-        const getStudentdata = () => {
-            const resData = [
-                {
-                    "subject": "Hindi",
-                    "marks": "65"
-                },
-                {
-                    "subject": "Math",
-                    "marks": "76"
-                },
-                {
-                    "subject": "English",
-                    "marks": "85"
-                },
-                {
-                    "subject": "Science",
-                    "marks": "65"
-                },
-                {
-                    "subject": "SocialScience",
-                    "marks": "64"
-                }
-            ]
-            for (let i = 0; i < resData.length; i++) {
-                sSubject.push(resData[i].subject);
-                sMarks.push(parseInt(resData[i].marks));
-            }
-            setStudentsubject(sSubject);
-            setStudentMarks(sMarks);
-            //console.log(resData); 
-        }
-
-        getStudentdata();
-
-    }, []);
-
     return (
         <React.Fragment>
-            <div  >
-                <h3 className="mt-3">Welcome to Piechart </h3>
+            <div className="aaa">
+                a
                 <Chart
+                    options={{
+                        labels: [
+                            "Monday",
+                            "Tuesday",
+                            "Wednesday",
+                            "Thursday",
+                            "Friday",
+                            "Saturday"
+                        ],
+                        theme: {
+                            monochrome: {
+                                enabled: false
+                            }
+                        },
+                        responsive: [{
+                            breakpoint: 480,
+                            options: {
+                                chart: {
+                                    width: "100%"
+                                },
+                                legend: {
+                                    show: false
+                                }
+                            }
+                        }]
+                    }}
+                    series={[44, 55, 41, 17, 15]}
                     type="pie"
-                    width={1349}
-                    height={550}
-
-                    series={studentMarks}
-
-                >
-                </Chart>
+                    width="300" />
             </div>
         </React.Fragment>
     );
